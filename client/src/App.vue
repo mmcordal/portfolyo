e5b0ee7e52c05dd0381ec9b72727d1fa75aea2cd
mcordal <mmcordal123@gmail.com>
2026-03-07 12:02:58 +0300

    yeni eklenen dosyalar (frontend)

---
+++ client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <!-- Navbar sadece auth sayfaları dışındaysa gösterilecek -->
+    <Navbar v-if="showNavbar" />
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import Navbar from "./components/Navbar.vue";
+
+const route = useRoute();
+
+// Auth sayfaları hariç Navbar göster
+const showNavbar = computed(() => {
+  return !(route.path === "/login" || route.path === "/register");
+});
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
